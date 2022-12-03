5799ffeb2714f2d6cf518a45fdf412b212dde11f
thupham16 <thu-pta@jacobins-digital.com>
2022-12-03 23:40:48 +0700

    sales CTE

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,33 @@
+WITH fact_sales_order_line_source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+
+),
+
+  fact_sales_rename_column AS (
+  SELECT
+    order_line_id AS sales_order_line_key,
+    stock_item_id AS product_key,
+    quantity,
+    unit_price     
+  FROM fact_sales_order_line_source
+),
+
+ fact_sales_cast_type AS (
+  SELECT
+    CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key,
+    CAST (quantity AS INTEGER) AS quantity,
+    CAST (unit_price AS NUMERIC) AS unit_price,
+    CAST(product_key AS INTEGER) AS product_key
+  FROM fact_sales_rename_column
+)
+
 SELECT 
-  CAST(order_line_id AS INTEGER) AS sales_order_line_key,
-  CAST (quantity AS INTEGER) AS quantity,
-  CAST (unit_price AS NUMERIC) AS unit_price,
-  CAST (quantity AS INTEGER) * CAST(unit_price AS NUMERIC) AS gross_amount,
-  CAST(stock_item_id AS INTEGER) AS product_key
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  sales_order_line_key,
+  quantity,
+  unit_price,
+  product_key,
+  quantity * unit_price AS gross_amount
+FROM fact_sales_cast_type
+
+  
