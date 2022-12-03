22f405b23900ea27e71cedfa529ef83e13b55901
thupham16 <thu-pta@jacobins-digital.com>
2022-12-03 21:33:41 +0700

    add product_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,5 +2,6 @@ SELECT
   order_line_id AS sales_order_line_key,
   quantity,
   unit_price,
-  (quantity*unit_price) AS gross_amount
+  (quantity*unit_price) AS gross_amount,
+  stock_item_id AS product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
