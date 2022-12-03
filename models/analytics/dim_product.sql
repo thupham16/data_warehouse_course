3f3fc99a2973ace16e38f048b17f751ee6e68113
thupham16 <thu-pta@jacobins-digital.com>
2022-12-03 23:21:09 +0700

    CTE rename column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,18 @@
+WITH dim_product__souce AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+),
+  dim_product__renamecolumn AS (
+  SELECT 
+    stock_item_id AS product_key,
+    stock_item_name AS product_name,
+    brand AS brand_name
+  FROM dim_product__souce
+  )
+
 SELECT 
-  CAST(stock_item_id AS INTEGER)  AS product_key,
-  CAST(stock_item_name AS STRING ) AS product_name,
-  CAST (brand AS STRING) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  CAST(product_key AS INTEGER) as product_key,
+  CAST(product_name AS STRING) as product_name,
+  CAST (brand_name AS STRING) as brand_name
+FROM dim_product__renamecolumn
+
