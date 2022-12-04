73fb78ec4e1957845b332cad8731da56a7fe087e
thupham16 <thu-pta@jacobins-digital.com>
2022-12-04 23:38:42 +0700

    add custoer and buying key

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -6,18 +6,24 @@ WITH dim_customer_source AS (
   dim_customer_rename AS (
     SELECT  
       customer_id AS customer_key,
-      customer_name 
+      customer_name ,
+      customer_category_id AS customer_category_key,
+      buying_group_id AS buying_group_key
     FROM dim_customer_source
  ),
   
   dim_customer_cast_type AS (
     SELECT 
       CAST (customer_key AS INTEGER) AS customer_key,
-      CAST (customer_name AS STRING) AS customer_name
+      CAST (customer_name AS STRING) AS customer_name,
+      CAST (customer_category_key AS INTEGER) AS customer_category_key,
+      CAST (buying_group_key AS INTEGER) AS buying_group_key
     FROM dim_customer_rename
   )
 
 SELECT 
   customer_key,
-  customer_name 
+  customer_name,
+  customer_category_key,
+  buying_group_key
 FROM dim_customer_cast_type
