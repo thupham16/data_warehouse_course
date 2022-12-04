604c29f0b2c29134d1cb746124ba361611e39059
thupham16 <thu-pta@jacobins-digital.com>
2022-12-04 17:56:36 +0700

    CTE fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,33 +1,37 @@
-WITH fact_sales_order_line_source AS (
+WITH fact_sales_order_line__source AS (
   SELECT *
   FROM `vit-lam-data.wide_world_importers.sales__order_lines`
 
 ),
 
-  fact_sales_rename_column AS (
+  fact_sales_order_line__rename_column AS (
   SELECT
     order_line_id AS sales_order_line_key,
     stock_item_id AS product_key,
     quantity,
-    unit_price     
-  FROM fact_sales_order_line_source
+    unit_price,
+    order_id AS sales_order_key
+  FROM fact_sales_order_line__source
 ),
 
- fact_sales_cast_type AS (
+ fact_sales_order_line__cast_type AS (
   SELECT
     CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key,
     CAST (quantity AS INTEGER) AS quantity,
     CAST (unit_price AS NUMERIC) AS unit_price,
-    CAST(product_key AS INTEGER) AS product_key
-  FROM fact_sales_rename_column
-)
+    CAST(product_key AS INTEGER) AS product_key,
+    CAST(sales_order_key AS INTEGER) AS sales_order_key
+  FROM fact_sales_order_line__rename_column
+)  
 
 SELECT 
   sales_order_line_key,
   quantity,
   unit_price,
   product_key,
-  quantity * unit_price AS gross_amount
-FROM fact_sales_cast_type
+  quantity * unit_price AS gross_amount,
+  sales_order_key
+FROM fact_sales_order_line__cast_type
+
 
   
