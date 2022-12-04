a6f7db5949e3c391e347e386b0a20330c7868e60
thupham16 <thu-pta@jacobins-digital.com>
2022-12-04 22:55:16 +0700

    supplier key and name

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,25 @@
+WITH dim_purchasing_supplier__source AS (
+      SELECT 
+        *
+      FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+),
+
+    dim_purchasing_supplier__rename_column AS (
+      SELECT 
+        supplier_id AS supplier_key,
+        supplier_name
+      FROM dim_purchasing_supplier__source
+),
+
+    dim_purchasing_supplier__cast_type AS (
+      SELECT 
+        CAST (supplier_key AS INTEGER) AS supplier_key,
+        CAST (supplier_name AS STRING) AS supplier_name   
+      FROM dim_purchasing_supplier__rename_column
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  supplier_key,
+  supplier_name
+FROM dim_purchasing_supplier__cast_type
+
