f963903d08f22b23adc02f015a487c3cb7db590c
thupham16 <thu-pta@jacobins-digital.com>
2022-12-04 16:41:23 +0700

    CTE customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,23 @@
+WITH dim_customer_source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__customers`
+),
+  
+  dim_customer_rename AS (
+    SELECT  
+      customer_id AS customer_key,
+      customer_name 
+    FROM dim_customer_source
+ ),
+  
+  dim_customer_cast_type AS (
+    SELECT 
+      CAST (customer_key AS INTEGER) AS customer_key,
+      CAST (customer_name AS STRING) AS customer_name
+    FROM dim_customer_rename
+  )
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+  customer_key,
+  customer_name 
+FROM dim_customer_cast_type
