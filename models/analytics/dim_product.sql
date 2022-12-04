93d6847ad0756a67ae3deca67fc3d9fd8f908905
thupham16 <thu-pta@jacobins-digital.com>
2022-12-04 23:28:22 +0700

    add supplier name

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -6,20 +6,25 @@ WITH dim_product__souce AS (
   SELECT 
     stock_item_id AS product_key,
     stock_item_name AS product_name,
-    brand AS brand_name
+    brand AS brand_name,
+    supplier_id AS supplier_key
   FROM dim_product__souce
   ),
-  dim_product_cast_type AS (
+  dim_product__cast_type AS (
   SELECT 
     CAST(product_key AS INTEGER) as product_key,
     CAST(product_name AS STRING) as product_name,
-    CAST (brand_name AS STRING) as brand_name
+    CAST (brand_name AS STRING) as brand_name,
+    CAST (supplier_key AS INTEGER) AS supplier_key
   FROM dim_product__renamecolumn
   )
 
 SELECT 
-  product_key,
-  product_name,
-  brand_name
-FROM dim_product_cast_type
-
+  dim_product.product_key,
+  dim_product.product_name,
+  dim_product.brand_name,
+  dim_product.supplier_key,
+  dim_supplier.supplier_name
+FROM dim_product__cast_type AS dim_product
+LEFT JOIN {{ ref ('dim_supplier')}} AS dim_supplier
+ON dim_product.supplier_key = dim_supplier.supplier_key
