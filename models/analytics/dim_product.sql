bcc2b3a6cc737705ccf7f9447820cba478c06d6f
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 10:33:26 +0700

    convert boolean

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -19,7 +19,19 @@ WITH dim_product__souce AS (
     CAST (supplier_key AS INTEGER) AS supplier_key,
     CAST (is_chiller_stock AS BOOLEAN) AS is_chiller_stock
   FROM dim_product__renamecolumn
+  ),
+
+  dim_product__convert_boolean AS (
+  SELECT *,
+    CASE 
+      WHEN is_chiller_stock IS TRUE THEN 'Chiller Stock'
+      WHEN is_chiller_stock IS FALSE THEN 'NOT Chiller Stock'
+    ELSE 'Undefined'
+    END AS chiller_stock
+
+  FROM dim_product__cast_type
   )
+
 
 SELECT 
   dim_product.product_key,
@@ -27,7 +39,7 @@ SELECT
   dim_product.brand_name,
   dim_product.supplier_key,
   dim_supplier.supplier_name,
-  dim_product.is_chiller_stock
-FROM dim_product__cast_type AS dim_product
+  dim_product.chiller_stock
+FROM dim_product__convert_boolean AS dim_product
 LEFT JOIN {{ ref ('dim_supplier') }} AS dim_supplier
 ON dim_product.supplier_key = dim_supplier.supplier_key
