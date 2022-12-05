bee11d997eb50cd0bedc0c3704fdd319c6ddcd40
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 13:58:53 +0700

    create dim person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,24 @@
+
+WITH app_people__source AS (
+  SELECT  *
+  FROM `vit-lam-data.wide_world_importers.application__people`
+),
+
+  app_people__rename AS (
+  SELECT 
+    person_id AS person_key,
+    full_name
+  FROM app_people__source
+  ),
+
+  app_people__concast_type AS (
+  SELECT 
+    CAST (person_key AS INTEGER) AS person_key,
+    CAST (full_name AS STRING) AS full_name
+  FROM app_people__rename
+  )
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+  person_key,
+  full_name 
+FROM app_people__concast_type
