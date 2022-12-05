2062f590d29a295a99ef102ad95d3f72cbcb3b78
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 11:56:36 +0700

    coalesce left join

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -32,13 +32,12 @@ WITH dim_product__souce AS (
   FROM dim_product__cast_type
   )
 
-
 SELECT 
   dim_product.product_key,
   dim_product.product_name,
   COALESCE(dim_product.brand_name, 'Undefined') AS brand_name,
   dim_product.supplier_key,
-  dim_supplier.supplier_name,
+  COALESCE(dim_supplier.supplier_name,'Undefined') AS supplier_name,
   dim_product.is_chiller_stock
 FROM dim_product__convert_boolean AS dim_product
 LEFT JOIN {{ ref ('dim_supplier') }} AS dim_supplier
