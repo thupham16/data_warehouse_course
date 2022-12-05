0a3c56d60f83348d4cbe8156c18ea3fcd41cc258
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 15:17:25 +0700

    add undefined row

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -16,9 +16,22 @@ WITH dim_person__source AS (
     CAST (person_key AS INTEGER) AS person_key,
     CAST (full_name AS STRING) AS full_name
   FROM dim_person__rename
+  ),
+
+  dim_person__add_undefined_record AS (
+  SELECT 
+    person_key,
+    full_name
+  FROM dim_person__concast_type
+  
+  UNION ALL
+
+  SELECT 
+    0 AS person_key,
+    'Undefined' AS full_name
   )
 
 SELECT 
   person_key,
   full_name 
-FROM dim_person__concast_type
+FROM dim_person__add_undefined_record
