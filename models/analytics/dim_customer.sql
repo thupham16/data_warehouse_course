5d72f23909fe7fe5c649987bff6beea40bbd6b6c
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 10:50:54 +0700

    add credit hold

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,24 +1,37 @@
-WITH dim_customer_source AS (
+WITH dim_customer__source AS (
     SELECT *
     FROM `vit-lam-data.wide_world_importers.sales__customers`
 ),
   
-  dim_customer_rename AS (
+  dim_customer__rename AS (
     SELECT  
       customer_id AS customer_key,
       customer_name ,
       customer_category_id AS customer_category_key,
-      buying_group_id AS buying_group_key
-    FROM dim_customer_source
+      buying_group_id AS buying_group_key,
+      is_on_credit_hold AS is_on_credit_hold_boolean
+    FROM dim_customer__source
  ),
   
-  dim_customer_cast_type AS (
+  dim_customer__cast_type AS (
     SELECT 
       CAST (customer_key AS INTEGER) AS customer_key,
       CAST (customer_name AS STRING) AS customer_name,
       CAST (customer_category_key AS INTEGER) AS customer_category_key,
-      CAST (buying_group_key AS INTEGER) AS buying_group_key
-    FROM dim_customer_rename
+      CAST (buying_group_key AS INTEGER) AS buying_group_key,
+      CAST (is_on_credit_hold_boolean AS BOOLEAN) AS is_on_credit_hold_boolean
+    FROM dim_customer__rename
+  ),
+
+  dim_customer__convert_boolean AS (
+    SELECT *,
+      CASE 
+        WHEN is_on_credit_hold_boolean IS TRUE THEN 'On Credit Hold'
+        WHEN is_on_credit_hold_boolean IS FALSE THEN 'Not On Credit Hold'
+        ELSE 'Undefined'
+        END AS is_on_credit_hold
+
+    FROM dim_customer__cast_type
   )
 
 SELECT 
@@ -27,11 +40,12 @@ SELECT
   dim_customer.customer_category_key,
   dim_customer_category.customer_category_name,
   dim_customer.buying_group_key,
-  dim_buying_group.buying_group_name
-FROM dim_customer_cast_type AS dim_customer 
+  dim_buying_group.buying_group_name,
+  dim_customer.is_on_credit_hold
+FROM dim_customer__convert_boolean AS dim_customer 
 LEFT JOIN {{ ref ('stg_dim_customer_category') }} AS dim_customer_category 
 ON dim_customer_category.customer_category_key = dim_customer.customer_category_key
 
-LEFT JOIN {{ ref ('stg_dim_sales_buying_group') }} AS dim_buying_group
+LEFT JOIN {{ ref ('stg_dim_sales_buying_group')}} AS dim_buying_group
 ON dim_buying_group.buying_group_key = dim_customer.buying_group_key
 
