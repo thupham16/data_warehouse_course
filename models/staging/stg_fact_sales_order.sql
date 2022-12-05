603fede19e4c6969f30bd2774b2ef2b63a6357d1
thupham16 <thu-pta@jacobins-digital.com>
2022-12-05 14:22:38 +0700

    update column name pick by person

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -7,7 +7,7 @@ WITH fact_sales_order__source AS (
     SELECT 
       order_id AS sales_order_key,
       customer_id AS customer_key,
-      picked_by_person_id AS pick_person_key
+      picked_by_person_id AS picked_by_person_key
     FROM fact_sales_order__source
   ),
 
@@ -15,12 +15,12 @@ WITH fact_sales_order__source AS (
     SELECT 
       CAST(sales_order_key AS INTEGER) AS sales_order_key,
       CAST (customer_key AS INTEGER) AS customer_key,
-      CAST (pick_person_key AS INTEGER) AS pick_person_key
+      CAST (picked_by_person_key AS INTEGER) AS picked_by_person_key
     FROM fact_sales_order__rename_column
   )
 
 SELECT 
   sales_order_key,
   customer_key,
-  pick_person_key
+  picked_by_person_key
 FROM fact_sales_order__cast_type
