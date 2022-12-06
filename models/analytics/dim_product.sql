cfaa78e63c4e4b7c43c98f5d061c84e102018d03
thupham16 <thu-pta@jacobins-digital.com>
2022-12-06 18:07:01 +0700

    add all columns

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -6,18 +6,30 @@ WITH dim_product__souce AS (
   SELECT 
     stock_item_id AS product_key,
     stock_item_name AS product_name,
+    is_chiller_stock,
+    lead_time_days,
+    quantity_per_outer,
     brand AS brand_name,
     supplier_id AS supplier_key,
-    is_chiller_stock
+    color_id AS color_key,
+    unit_package_id AS unit_package_key,
+    outer_package_id AS outer_package_key
+
   FROM dim_product__souce
   ),
   dim_product__cast_type AS (
   SELECT 
     CAST(product_key AS INTEGER) as product_key,
     CAST(product_name AS STRING) as product_name,
+    CAST (is_chiller_stock AS BOOLEAN) AS is_chiller_stock_boolean,
+    CAST(lead_time_days AS INTEGER) AS lead_time_days,
+    CAST(quantity_per_outer AS INTEGER) AS quantity_per_outer,
     CAST (brand_name AS STRING) as brand_name,
     CAST (supplier_key AS INTEGER) AS supplier_key,
-    CAST (is_chiller_stock AS BOOLEAN) AS is_chiller_stock_boolean
+    CAST (color_key AS INTEGER) AS color_key,
+    CAST (unit_package_key AS INTEGER) AS unit_package_key,
+    CAST (outer_package_key AS INTEGER) AS outer_package_key
+
   FROM dim_product__renamecolumn
   ),
 
@@ -35,10 +47,29 @@ WITH dim_product__souce AS (
 SELECT 
   dim_product.product_key,
   dim_product.product_name,
+  dim_product.is_chiller_stock,
+  dim_product.lead_time_days,
+  dim_product.quantity_per_outer,
   COALESCE(dim_product.brand_name, 'Undefined') AS brand_name,
   dim_product.supplier_key,
   COALESCE(dim_supplier.supplier_name,'Undefined') AS supplier_name,
-  dim_product.is_chiller_stock
+  dim_product.color_key,
+  COALESCE(dim_color.color_name,'Undefined') AS color_name,
+  dim_product.unit_package_key,
+  COALESCE(dim_unit_package_type.package_type_name,'Undefined') AS unit_package_type_name,
+  dim_product.outer_package_key,
+  COALESCE(dim_outer_package_type.package_type_name,'Undefined') AS outer_package_type_name
+
 FROM dim_product__convert_boolean AS dim_product
-LEFT JOIN {{ ref ('dim_supplier') }} AS dim_supplier
+
+LEFT JOIN {{ref ('dim_supplier') }} AS dim_supplier
 ON dim_product.supplier_key = dim_supplier.supplier_key
+
+LEFT JOIN vit-lam-data.wide_world_importers.warehouse__colors AS dim_color
+ON dim_product.color_key = dim_color.color_id
+
+LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_unit_package_type
+ON dim_product.unit_package_key = dim_unit_package_type.package_type_key
+
+LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_outer_package_type
+ON dim_product.outer_package_key = dim_outer_package_type.package_type_key
