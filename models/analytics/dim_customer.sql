1196af9b088e75d463596929425cdadc7c5eb5b5
thupham16 <thu-pta@jacobins-digital.com>
2022-12-06 17:32:44 +0700

    add all columns

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,9 +7,14 @@ WITH dim_customer__source AS (
     SELECT  
       customer_id AS customer_key,
       customer_name ,
+      is_statement_sent AS is_statement_sent_boolean,
+      is_on_credit_hold AS is_on_credit_hold_boolean,
+      payment_days,
       customer_category_id AS customer_category_key,
       buying_group_id AS buying_group_key,
-      is_on_credit_hold AS is_on_credit_hold_boolean
+      delivery_method_id AS delivery_method_key,
+      delivery_city_id AS delivery_city_key,
+      account_opened_date
     FROM dim_customer__source
  ),
   
@@ -17,9 +22,14 @@ WITH dim_customer__source AS (
     SELECT 
       CAST (customer_key AS INTEGER) AS customer_key,
       CAST (customer_name AS STRING) AS customer_name,
+      CAST (is_statement_sent_boolean AS BOOLEAN) AS is_statement_sent_boolean,
+      CAST (is_on_credit_hold_boolean AS BOOLEAN) AS is_on_credit_hold_boolean,
+      CAST (payment_days AS INTEGER) AS payment_days,
       CAST (customer_category_key AS INTEGER) AS customer_category_key,
       CAST (buying_group_key AS INTEGER) AS buying_group_key,
-      CAST (is_on_credit_hold_boolean AS BOOLEAN) AS is_on_credit_hold_boolean
+      CAST (delivery_method_key AS INTEGER) AS delivery_method_key,
+      CAST(delivery_city_key AS INTEGER) AS delivery_city_key,
+      CAST(account_opened_date AS date) AS account_opened_date
     FROM dim_customer__rename
   ),
 
@@ -29,7 +39,12 @@ WITH dim_customer__source AS (
         WHEN is_on_credit_hold_boolean IS TRUE THEN 'On Credit Hold'
         WHEN is_on_credit_hold_boolean IS FALSE THEN 'Not On Credit Hold'
         ELSE 'Undefined'
-        END AS is_on_credit_hold
+        END AS is_on_credit_hold,
+      CASE 
+        WHEN is_statement_sent_boolean IS TRUE THEN 'Statement Sent'
+        WHEN is_statement_sent_boolean IS FALSE THEN 'Not Statement Sent'
+        ELSE 'Undefined'
+        END AS is_statement_sent
 
     FROM dim_customer__cast_type
   )
@@ -37,15 +52,30 @@ WITH dim_customer__source AS (
 SELECT 
   dim_customer.customer_key,
   dim_customer.customer_name,
+  dim_customer.is_statement_sent,
+  dim_customer.is_on_credit_hold,
+  dim_customer.payment_days,
   dim_customer.customer_category_key,
   COALESCE(dim_customer_category.customer_category_name,'Undefined') AS customer_category_name,
   dim_customer.buying_group_key,
   COALESCE(dim_buying_group.buying_group_name,'Undefined') AS buying_group_name,
-  dim_customer.is_on_credit_hold
+  dim_customer.delivery_method_key,
+  COALESCE(dim_delivery_method.delivery_method_name,'Undefined') AS delivery_method_name,
+  dim_customer.delivery_city_key,
+  COALESCE(dim_city_state_province.city_name,'Undefined') AS delivery_city_name,
+  dim_city_state_province.state_province_key,
+  COALESCE(dim_city_state_province.state_province_name,'Undefined') AS delivery_state_name,
+  dim_customer.account_opened_date
+
 FROM dim_customer__convert_boolean AS dim_customer 
-LEFT JOIN {{ ref ('stg_dim_customer_category') }} AS dim_customer_category 
+LEFT JOIN {{ref ('stg_dim_customer_category')}} AS dim_customer_category 
 ON dim_customer_category.customer_category_key = dim_customer.customer_category_key
 
-LEFT JOIN {{ ref ('stg_dim_sales_buying_group')}} AS dim_buying_group
+LEFT JOIN {{ref ('stg_dim_sales_buying_group')}} AS dim_buying_group
 ON dim_buying_group.buying_group_key = dim_customer.buying_group_key
 
+LEFT JOIN {{ref('stg_dim_delivery_method')}} AS dim_delivery_method
+ON dim_delivery_method.delivery_method_key = dim_customer.delivery_method_key
+
+LEFT JOIN {{ref ('stg_dim_city_state_province')}} AS dim_city_state_province
+ON dim_city_state_province.city_key = dim_customer.delivery_city_key
