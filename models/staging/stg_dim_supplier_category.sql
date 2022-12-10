cc43537ba8abf3d88b6899d0fd81135889f0b38c
thupham16 <thu-pta@jacobins-digital.com>
2022-12-10 16:45:36 +0700

    add dim supplier category

--- models/staging/stg_dim_supplier_category.sql
+++ models/staging/stg_dim_supplier_category.sql
@@ -13,7 +13,7 @@ WITH dim_supplier_category__source AS (
   dim__supplier_categoryy__cast_type AS (
   SELECT
     CAST (supplier_category_key AS INTEGER) AS supplier_category_key,
-    CAST (supplier_category_name AS  STRING) AS supplier_category_name
+    CAST (supplier_category_name AS STRING) AS supplier_category_name
   FROM dim_supplier_category__rename
   )
 
