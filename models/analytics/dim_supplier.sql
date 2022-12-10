206da678bc4bb1297268b783a7dc7510f48ed3d4
thupham16 <thu-pta@jacobins-digital.com>
2022-12-10 16:45:52 +0700

    add supplier category

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -7,18 +7,24 @@ WITH dim_purchasing_supplier__source AS (
     dim_purchasing_supplier__rename_column AS (
       SELECT 
         supplier_id AS supplier_key,
-        supplier_name
+        supplier_name,
+        supplier_category_id AS supplier_category_key
       FROM dim_purchasing_supplier__source
 ),
 
     dim_purchasing_supplier__cast_type AS (
       SELECT 
         CAST (supplier_key AS INTEGER) AS supplier_key,
-        CAST (supplier_name AS STRING) AS supplier_name   
+        CAST (supplier_name AS STRING) AS supplier_name,
+        CAST (supplier_category_key AS INTEGER) AS supplier_category_key   
       FROM dim_purchasing_supplier__rename_column
 )
 
 SELECT 
-  supplier_key,
-  supplier_name
-FROM dim_purchasing_supplier__cast_type 
+  dim_supplier.supplier_key,
+  dim_supplier.supplier_name,
+  dim_supplier.supplier_category_key,
+  dim_supplier_category.supplier_category_name
+FROM dim_purchasing_supplier__cast_type AS dim_supplier
+JOIN {{ ref ('stg_dim_supplier_category')}} AS dim_supplier_category
+ON dim_supplier.supplier_category_key = dim_supplier_category.supplier_category_key
