62ba38c26c7223ba69a859c40289a7dc4968d049
thupham16 <thu-pta@jacobins-digital.com>
2022-12-10 17:23:37 +0700

    add dim salesperson

--- models/analytics/role_playing_dimensions/dim_salesperson_person.sql
+++ models/analytics/role_playing_dimensions/dim_salesperson_person.sql
@@ -1,7 +1,9 @@
 SELECT 
   person_key AS salesperson_key,
   full_name AS salesperson_full_name,
-  search_name AS salesperson_search_name
+  search_name AS salesperson_search_name,
+  is_system_user,
+  is_employee,
+  is_salesperson
 
 FROM {{ref ('dim_person')}}
-WHERE is_salesperson = 'Salesperson' 
