6741be564aa599e3f03e4810d5653327cf54a70c
thupham16 <thu-pta@jacobins-digital.com>
2022-12-14 19:52:15 +0700

    farmfingerprint indicator key

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -7,13 +7,14 @@ UNION ALL
 )
 
 SELECT  
-  CONCAT(dim_is_undersupply_backordered.is_undersupply_backordered,
-  ',',
-  dim_package_type.package_type_key
-  ) AS sales_order_line_indicator_key,
+  FARM_FINGERPRINT(
+    CONCAT(dim_is_undersupply_backordered.is_undersupply_backordered,
+    ',',
+    dim_package_type.package_type_key
+    )) AS sales_order_line_indicator_key,
   is_undersupply_backordered,
   dim_package_type.package_type_key,
   dim_package_type.package_type_name
 FROM dim_is_undersupply_backordered
-CROSS JOIN data-modeling-370410.wide_world_importers_dwh_staging.stg_dim_package_type AS dim_package_type
+CROSS JOIN {{ref('stg_dim_package_type')}} AS dim_package_type
 
