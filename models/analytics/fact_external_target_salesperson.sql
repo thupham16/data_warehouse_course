8654e175b6599076b983fecef3f44ca64fc74afb
thupham16 <thu-pta@jacobins-digital.com>
2022-12-21 20:35:00 +0700

    Clean fact target sales person

---
+++ models/analytics/fact_external_target_salesperson.sql
@@ -0,0 +1,29 @@
+WITH fact_external_target_salesperson__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.external__salesperson_target`
+)
+
+, fact_external_target_salesperson__rename_column AS (
+  SELECT
+    year_month
+    , salesperson_person_id AS salesperson_person_key
+    , target_revenue
+
+  FROM fact_external_target_salesperson__source
+)
+
+, fact_external_target_salesperson__cast_type AS (
+  SELECT 
+    CAST(year_month AS DATE) AS year_month
+    , CAST(salesperson_person_key AS INTEGER) AS salesperson_person_key
+    , CAST(target_revenue AS NUMERIC) AS target_revenue
+
+  FROM fact_external_target_salesperson__rename_column
+)
+
+SELECT 
+  year_month
+  , salesperson_person_key
+  , target_revenue
+
+FROM fact_external_target_salesperson__cast_type
