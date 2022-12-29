5e4581f60f07c210d2369138fc1f36a9ec6f4e46
thupham16 <thu-pta@jacobins-digital.com>
2022-12-29 21:17:38 +0700

    flatten external category

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -44,36 +44,62 @@ WITH dim_product__souce AS (
   FROM dim_product__cast_type
   )
 
+, dim_product__join_1 AS (
+    SELECT 
+      dim_product.product_key,
+      dim_product.product_name,
+      dim_product.is_chiller_stock,
+      dim_product.lead_time_days,
+      dim_product.quantity_per_outer,
+      COALESCE(dim_product.brand_name, 'Undefined') AS brand_name,
+      dim_product.supplier_key,
+      COALESCE(dim_supplier.supplier_name,'Undefined') AS supplier_name,
+      dim_product.color_key,
+      COALESCE(dim_color.color_name,'Undefined') AS color_name,
+      dim_product.unit_package_type_key,
+      COALESCE(dim_unit_package_type.package_type_name,'Undefined') AS unit_package_type_name,
+      dim_product.outer_package_type_key,
+      COALESCE(dim_outer_package_type.package_type_name,'Undefined') AS outer_package_type_name
+      , dim_external_stock_item.category_key
+      
+    FROM dim_product__convert_boolean AS dim_product
+
+    LEFT JOIN {{ref ('dim_supplier') }} AS dim_supplier
+      ON dim_product.supplier_key = dim_supplier.supplier_key
+
+    LEFT JOIN vit-lam-data.wide_world_importers.warehouse__colors AS dim_color
+      ON dim_product.color_key = dim_color.color_id
+
+    LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_unit_package_type
+      ON dim_product.unit_package_type_key = dim_unit_package_type.package_type_key
+
+    LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_outer_package_type
+      ON dim_product.outer_package_type_key = dim_outer_package_type.package_type_key
+
+    LEFT JOIN {{ref('stg_dim_external_stock_item')}} AS dim_external_stock_item
+      USING (product_key)
+)
+
 SELECT 
-  dim_product.product_key,
-  dim_product.product_name,
-  dim_product.is_chiller_stock,
-  dim_product.lead_time_days,
-  dim_product.quantity_per_outer,
-  COALESCE(dim_product.brand_name, 'Undefined') AS brand_name,
-  dim_product.supplier_key,
-  COALESCE(dim_supplier.supplier_name,'Undefined') AS supplier_name,
-  dim_product.color_key,
-  COALESCE(dim_color.color_name,'Undefined') AS color_name,
-  dim_product.unit_package_type_key,
-  COALESCE(dim_unit_package_type.package_type_name,'Undefined') AS unit_package_type_name,
-  dim_product.outer_package_type_key,
-  COALESCE(dim_outer_package_type.package_type_name,'Undefined') AS outer_package_type_name
-  , dim_external_stock_item.category_key
-  
-FROM dim_product__convert_boolean AS dim_product
+  product_key
+  , product_name
+  , is_chiller_stock
+  , lead_time_days
+  , quantity_per_outer
+  , brand_name
+  , supplier_key
+  , supplier_name
+  , color_key
+  , color_name
+  , unit_package_type_key
+  , unit_package_type_name
+  , outer_package_type_key
+  , outer_package_type_name
+  , category_key
+  , COALESCE(dim_external_category.category_name,'Undefined') AS category_name
+  , COALESCE(dim_external_category.parent_category_key,'UNDEFINED') AS parent_category_key
+  , COALESCE(dim_external_category.category_level,'UNDEFINED') AS category_level
 
-LEFT JOIN {{ref ('dim_supplier') }} AS dim_supplier
-ON dim_product.supplier_key = dim_supplier.supplier_key
-
-LEFT JOIN vit-lam-data.wide_world_importers.warehouse__colors AS dim_color
-ON dim_product.color_key = dim_color.color_id
-
-LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_unit_package_type
-ON dim_product.unit_package_type_key = dim_unit_package_type.package_type_key
-
-LEFT JOIN {{ref ('stg_dim_package_type')}} AS dim_outer_package_type
-ON dim_product.outer_package_type_key = dim_outer_package_type.package_type_key
-
-LEFT JOIN {{ref('stg_dim_external_stock_item')}} AS dim_external_stock_item
-USING (product_key)
+FROM dim_product__join_1
+LEFT JOIN {{ref('stg_dim_external_category')}} AS dim_external_category
+USING (category_key)
