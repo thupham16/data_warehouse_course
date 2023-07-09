d63c8f875adc62e7a5d7235b774bd2c0f76c3680
thupham <thu.pta@jacobins-digital.com>
2023-07-09 21:54:32 +0700

    revise ,

--- models/analytics/role_playing_dimensions/dim_salesperson_person.sql
+++ models/analytics/role_playing_dimensions/dim_salesperson_person.sql
@@ -1,10 +1,10 @@
 SELECT 
-  person_key AS salesperson_key,
-  full_name AS salesperson_full_name,
-  search_name AS salesperson_search_name,
-  is_system_user,
-  is_employee,
-  is_salesperson
+  person_key AS salesperson_key
+  , full_name AS salesperson_full_name
+  , search_name AS salesperson_search_name
+  , is_system_user
+  , is_employee
+  , is_salesperson
 
 FROM {{ref ('dim_person')}}
 WHERE 
