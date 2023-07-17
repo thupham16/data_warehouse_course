22e8de951d68db78f87c4989e2a8d77c935205f1
thupham <thu.pta@jacobins-digital.com>
2023-07-18 00:10:26 +0700

    revise

--- models/analytics/fact_customer_snapshot.sql
+++ models/analytics/fact_customer_snapshot.sql
@@ -1,39 +1,40 @@
 WITH fact_customer_snapshot__source AS (
   SELECT 
     customer_key
-    , DATE_TRUNC(order_date, MONTH) AS order_date
+    , DATE_TRUNC(order_date, MONTH) AS year_month
     , SUM(gross_amount) AS sales_amount
-  FROM data-modeling-370410.wide_world_importers_dwh.fact_sales_order_line
+  FROM {{ref('fact_sales_order_line')}}
   GROUP BY 1, 2
 )
  
 , dim_year_month AS (
   SELECT DISTINCT year_month
-  FROM data-modeling-370410.wide_world_importers_dwh.dim_date
+  FROM {{ref('dim_date')}}
 )
 
 , fact_customer_snapshot__densed AS (
   SELECT DISTINCT customer_key
-  , year_month
+  , dim_year_month.year_month
   FROM fact_customer_snapshot__source
   CROSS JOIN dim_year_month
 )
 
 , fact_customer_snapshot__calculation  AS (
   SELECT 
-    fact_customer_snapshot__source.*
-    , SUM(fact_customer_snapshot__source.sales_amount) OVER (PARTITION BY fact_customer_snapshot__source.customer_key 
-    ORDER BY fact_customer_snapshot__source.order_date) AS life_time_sales_amount
-  FROM fact_customer_snapshot__source
-  LEFT JOIN fact_customer_snapshot__densed 
-    ON fact_customer_snapshot__densed.customer_key = fact_customer_snapshot__source.customer_key
-    AND fact_customer_snapshot__source.order_date = fact_customer_snapshot__densed.year_month
+    customer_key
+    , year_month
+    , COALESCE(fact_customer_snapshot__source.sales_amount, 0) AS sales_amount
+    , SUM(COALESCE(fact_customer_snapshot__source.sales_amount, 0)) OVER (PARTITION BY customer_key 
+    ORDER BY year_month) AS life_time_sales_amount
+  FROM fact_customer_snapshot__densed 
+  LEFT JOIN fact_customer_snapshot__source
+    USING (customer_key, year_month)
 )
 
 , fact_customer_snapshot__percentile AS (
   SELECT *
-  , PERCENT_RANK() OVER (PARTITION BY order_date ORDER BY sales_amount) AS sales_amount_percentile_rank
-  , PERCENT_RANK() OVER (PARTITION BY order_date ORDER BY life_time_sales_amount) AS life_time_sales_amount_percentile_rank  
+  , PERCENT_RANK() OVER (PARTITION BY year_month ORDER BY sales_amount) AS sales_amount_percentile_rank
+  , PERCENT_RANK() OVER (PARTITION BY year_month ORDER BY life_time_sales_amount) AS life_time_sales_amount_percentile_rank  
 
   FROM fact_customer_snapshot__calculation
 )
@@ -58,7 +59,7 @@ WITH fact_customer_snapshot__source AS (
 
 SELECT
   customer_key
-  , order_date
+  , year_month
   , sales_amount 
   , life_time_sales_amount
   , sales_amount_percentile_rank
