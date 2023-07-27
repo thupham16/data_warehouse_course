94e17f9de0bb4df9d8b4be831961991f0728686e
thupham <thu.pta@jacobins-digital.com>
2023-07-27 08:07:54 +0700

    add undefined

--- models/staging/stg_dim_external_category.sql
+++ models/staging/stg_dim_external_category.sql
@@ -21,23 +21,13 @@ WITH dim_external_category__source AS (
   FROM dim_external_category__rename
  )
 
-, dim_external_category__enrich AS (
-  SELECT
-    dim_category.*
-    , COALESCE(dim_parent_category.category_name, 'Invalid') AS parent_category_name
-  FROM dim_external_category__cast_type AS dim_category
-  LEFT JOIN dim_external_category__cast_type AS dim_parent_category
-    ON dim_category.parent_category_key = dim_parent_category.category_key
- )
-
 , dim_external_category__add_undefined_record AS (
    SELECT
     category_key
     , category_name
     , parent_category_key
-    , parent_category_name
     , category_level
-   FROM dim_external_category__enrich
+   FROM dim_external_category__cast_type
 
    UNION ALL
 
@@ -45,8 +35,16 @@ WITH dim_external_category__source AS (
    0 AS category_key
    , 'Undefined' AS category_name
    , 0 AS parent_category_key
-   , 'Undefined' AS parent_category_name
    , 0 AS category_level
+ )
+
+, dim_external_category__enrich AS (
+  SELECT
+    dim_category.*
+    , COALESCE(dim_parent_category.category_name, 'Invalid') AS parent_category_name
+  FROM dim_external_category__add_undefined_record AS dim_category
+  LEFT JOIN dim_external_category__add_undefined_record AS dim_parent_category
+    ON dim_category.parent_category_key = dim_parent_category.category_key
  )
 
 SELECT 
@@ -55,4 +53,4 @@ SELECT
   , parent_category_key
   , parent_category_name
   , category_level
-FROM dim_external_category__add_undefined_record
+FROM dim_external_category__enrich
