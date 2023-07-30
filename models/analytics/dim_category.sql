59d3a0a87f2e6fbb22c7ea7bed65ccbc9722ccdc
thupham <thu.pta@jacobins-digital.com>
2023-07-30 13:13:51 +0700

    revise

--- models/analytics/dim_category.sql
+++ models/analytics/dim_category.sql
@@ -1,4 +1,4 @@
-WITH dim_category__souce AS (
+WITH dim_category__source AS (
   SELECT *
   FROM {{ref('stg_dim_external_category')}}
 )
@@ -16,80 +16,65 @@ WITH dim_category__souce AS (
     , 0 AS category_key_level_4
     , 'Undefined' AS category_name_level_4
     , category_level
-  FROM dim_category__souce  
+  FROM dim_category__source  
   WHERE category_level = 1
-
 )
 
 , dim_category__level_2 AS (
   SELECT
-    dim_category_level_2.category_key
-    , dim_category_level_2.category_name
-    , dim_category_level_1.category_key AS category_key_level_1
-    , dim_category_level_1.category_name AS category_name_level_1
-    , dim_category_level_2.category_key AS category_key_level_2
-    , dim_category_level_2.category_name AS category_name_level_2
+    category_key
+    , category_name
+    , parent_category_key AS category_key_level_1
+    , parent_category_name AS category_name_level_1
+    , category_key AS category_key_level_2
+    , category_name AS category_name_level_2
     , 0 AS category_key_level_3
     , 'Undefined' AS category_name_level_3
     , 0 AS category_key_level_4
     , 'Undefined' AS category_name_level_4
-    , dim_category_level_2.category_level
-  FROM dim_category__souce AS dim_category_level_2
-  LEFT JOIN dim_category__souce AS dim_category_level_1
-    ON dim_category_level_1.category_key = dim_category_level_2.parent_category_key
-  WHERE dim_category_level_2.category_level = 2
+    , category_level
+  FROM dim_category__source  
+  WHERE category_level = 2
 )
 
 , dim_category__level_3 AS (
   SELECT
-    dim_category_level_3.category_key
-    , dim_category_level_3.category_name
-
-    , dim_category_level_1.category_key AS category_key_level_1
-    , dim_category_level_1.category_name AS category_name_level_1
-
-    , dim_category_level_2.category_key AS category_key_level_2
-    , dim_category_level_2.category_name AS category_name_level_2
-
-    , dim_category_level_3.category_key AS category_key_level_3
-    , dim_category_level_3.category_name AS category_name_level_3
+    category_level_3.category_key
+    , category_level_3.category_name
+    ,  category_level_1.parent_category_key AS category_key_level_1
+    ,  category_level_1.parent_category_name AS category_name_level_1
+    , category_level_3.parent_category_key AS category_key_level_2
+    , category_level_3.parent_category_name AS category_name_level_2
+    , category_level_3.category_key AS category_key_level_3
+    , category_level_3.category_name AS category_name_level_3
     , 0 AS category_key_level_4
     , 'Undefined' AS category_name_level_4
-    , dim_category_level_3.category_level
-  FROM dim_category__souce AS dim_category_level_3
-  LEFT JOIN dim_category__souce AS dim_category_level_2
-    ON dim_category_level_2.category_key = dim_category_level_3.parent_category_key
-  LEFT JOIN dim_category__souce AS dim_category_level_1
-    ON dim_category_level_1.category_key = dim_category_level_3.parent_category_key
-  WHERE dim_category_level_3.category_level = 3
+    , category_level_3.category_level
+  FROM dim_category__source AS category_level_3
+  LEFT JOIN dim_category__source AS category_level_1
+    ON category_level_1.category_key = category_level_3.parent_category_key
+  WHERE category_level_3.category_level = 3
 )
 
 , dim_category__level_4 AS (
   SELECT
-    dim_category_level_4.category_key
-    , dim_category_level_4.category_name
-
-    , dim_category_level_1.category_key AS category_key_level_1
-    , dim_category_level_1.category_name AS category_name_level_1
-
-    ,  dim_category_level_2.category_key AS category_key_level_2
-    ,  dim_category_level_2.category_name AS category_name_level_2
-
-    , dim_category_level_3.category_key AS category_key_level_3
-    , dim_category_level_3.category_name AS category_name_level_3
-
-    , dim_category_level_4.category_key AS category_key_level_4
-    , dim_category_level_4.category_name AS category_name_level_4
-
-    , dim_category_level_4.category_level
-  FROM dim_category__souce AS dim_category_level_4
-  LEFT JOIN dim_category__souce AS dim_category_level_3
-    ON dim_category_level_3.category_key = dim_category_level_4.parent_category_key
-  LEFT JOIN dim_category__souce AS dim_category_level_2
-    ON dim_category_level_2.category_key = dim_category_level_3.parent_category_key
-  LEFT JOIN dim_category__souce AS dim_category_level_1
-    ON dim_category_level_1.category_key = dim_category_level_2.parent_category_key
-  WHERE dim_category_level_4.category_level = 4
+    category_level_4.category_key
+    , category_level_4.category_name
+    ,  category_level_1.parent_category_key AS category_key_level_1
+    ,  category_level_1.parent_category_name AS category_name_level_1
+    , category_level_2.parent_category_key AS category_key_level_2
+    , category_level_2.parent_category_name AS category_name_level_2
+    , category_level_4.parent_category_key AS category_key_level_3
+    , category_level_4.parent_category_name AS category_name_level_3
+    , category_level_4.category_key AS category_key_level_4
+    , category_level_4.category_name AS category_name_level_4
+    , category_level_4.category_level
+  FROM dim_category__source AS category_level_4
+  LEFT JOIN dim_category__source AS category_level_2
+    ON category_level_2.category_key = category_level_4.parent_category_key
+  LEFT JOIN dim_category__source AS category_level_1
+    ON category_level_2.parent_category_key = category_level_1.category_key
+  WHERE category_level_4.category_level = 4
 )
 
 SELECT
